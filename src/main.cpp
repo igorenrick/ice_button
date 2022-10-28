a8712068d906350ab2bbf24eeafb561d6a76c346
Igor Enrick de Carvalho <igorenrickc@gmail.com>
2022-10-28 00:18:56 -0300

    remove timer

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,17 @@
 
 #define BUTTON 2
 #define RELE 9
-#define MIX_MAX_TIME 300000
-#define DELYA_TIME 3000
 
 unsigned long mixLastTime;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(BUTTON, INPUT);
   pinMode(RELE, OUTPUT);
-  mixLastTime = millis();
+  digitalWrite(RELE, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(BUTTON) == HIGH) {
-    // do something
+  if(digitalRead(BUTTON) == LOW) {
     digitalWrite(RELE, HIGH);
-  }
-
-  if ((millis() - mixLastTime) > MIX_MAX_TIME) {
-    delay(DELYA_TIME);
-    mixLastTime = millis();
   }
 }
